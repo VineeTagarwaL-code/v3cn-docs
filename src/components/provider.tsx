326020e5fdf8acf802ed88d01692166d748b6055
Badri-Vishal <badrivishalpadhy@gmail.com>
2025-03-27 12:08:06 +0530

    feat: update ThemeProvider to adjust themes based on pathname

--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -1,10 +1,18 @@
 "use client";
 
 import { ThemeProvider } from "@/components/theme-provider";
+import { usePathname } from "next/navigation";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+    <ThemeProvider 
+      attribute="class"
+      defaultTheme={pathname === '/' ? 'dark' : 'system'}
+      enableSystem={pathname !== '/'}
+      forcedTheme={pathname === '/' ? 'dark' : undefined}
+    >
       {children}
     </ThemeProvider>
   );
