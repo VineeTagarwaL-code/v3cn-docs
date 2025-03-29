53d319544033644a2fbf23ee8a95efdf0c0339d3
vineet agarwal <vineetagarwal@vineets-MacBook-Air.local>
2025-03-29 21:15:54 +0530

    feat: registry updated

---
+++ registry/v3cn/github-graph/github-graph-demo.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { GithubGraph } from "@/registry/v3cn/github-graph/github-graph";
+
+const GithubGraphDemo = () => {
+  return (
+    <GithubGraph
+      username="vineetagarwal-code"
+      blockMargin={2}
+      lightColorPalette={["#1e1e2f", "#5a3e7a", "#7e5aa2", "#a87cc3", "#d9a9e6"]}
+      darkColorPalette={["#1e1e2f", "#5a3e7a", "#7e5aa2", "#a87cc3", "#d9a9e6"]}
+    />
+  )
+}
+export { GithubGraphDemo };
