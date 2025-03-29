73583f3c2b5662898b0e9b7621cb929cbbcab07a
vineet agarwal <vineetagarwal@vineets-MacBook-Air.local>
2025-03-29 21:22:42 +0530

    feat: registry updated

--- registry/v3cn/github-graph/github-graph-demo.tsx
+++ registry/v3cn/github-graph/github-graph-demo.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { GithubGraph } from "@/registry/v3cn/github-graph/github-graph";
+// import { GithubGraph } from "@/components/github-graph";
 
 const GithubGraphDemo = () => {
   return (
